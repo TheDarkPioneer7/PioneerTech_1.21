d52a586f5cea0a5590c2aa529277c9d05b3e7019
TheDarkPioneer <jakobsenjakob07@gmail.com>
2025-07-16 16:35:02 +0200

    Foundation

--- src/main/java/net/tdp/pioneertech/PioneerTech.java
+++ src/main/java/net/tdp/pioneertech/PioneerTech.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PioneerTech implements ModInitializer {
-	public static final String MOD_ID = "test";
+	public static final String MOD_ID = "pioneertech";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
